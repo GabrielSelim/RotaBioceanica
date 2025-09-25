9d40532db2705bdd6f41690db2dacda550bfdb5c
Gabriel Sanz <gabriel.sanz@infortechms.com.br>
2025-09-25 14:57:45 -0400

    Remoção de classes e propriedades obsoletas
    Remoção de várias classes e propriedades em arquivos relacionados a entidades, incluindo `CartaPokemon`, `Contato`, `Cartao`, `Categoria`, `Lancamento`, `Parcelamento`, `ParcelamentoMensal`, `PessoaConta` e `Pessoa`, além de suas classes de validação.
    
    As alterações incluem a remoção de namespaces, atributos de coluna e propriedades desnecessárias. As interfaces de repositório foram atualizadas, eliminando métodos não utilizados, como `ObterPorIdSeguro` e `ObterTodosSeguro`. As classes de validação foram simplificadas, mantendo apenas as validações essenciais. A `ValidationServiceFactory` também foi ajustada para refletir essas mudanças, removendo instâncias de validação para as classes excluídas.

--- Projeto_Gabriel.Domain/Service/ValidationServiceFactory.cs
+++ Projeto_Gabriel.Domain/Service/ValidationServiceFactory.cs
@@ -1,9 +1,5 @@
 ﻿using Projeto_Gabriel.Domain.Entity;
-using Projeto_Gabriel.Domain.Entity.Contatos;
-using Projeto_Gabriel.Domain.Entity.Financas;
 using Projeto_Gabriel.Domain.Entity.Logas;
-using Projeto_Gabriel.Domain.Service.Contatos;
-using Projeto_Gabriel.Domain.Service.Financas;
 using Projeto_Gabriel.Domain.Service.Logas;
 using Projeto_Gabriel.Domain.ServiceInterface;
 
@@ -18,33 +14,6 @@ namespace Projeto_Gabriel.Infrastructure.Services.Validation
 
             if (typeof(T) == typeof(Usuario))
                 return (IEntityValidationService<T>)new UsuarioValidationService();
-
-            if (typeof(T) == typeof(Pessoa))
-                return (IEntityValidationService<T>)new PessoaValidationService();
-
-            if (typeof(T) == typeof(CartaPokemon))
-                return (IEntityValidationService<T>)new CartaPokemonValidationService();
-
-            if (typeof(T) == typeof(Cartao))
-                return (IEntityValidationService<T>)new CartaoValidationService();
-
-            if (typeof(T) == typeof(Categoria))
-                return (IEntityValidationService<T>)new CategoriaValidationService();
-
-            if (typeof(T) == typeof(Lancamento))
-                return (IEntityValidationService<T>)new LancamentoValidationService();
-
-            if (typeof(T) == typeof(Parcelamento))
-                return (IEntityValidationService<T>)new ParcelamentoValidationService();
-
-            if (typeof(T) == typeof(ParcelamentoMensal))
-                return (IEntityValidationService<T>)new ParcelamentoMensalValidationService();
-
-            if (typeof(T) == typeof(PessoaConta))
-                return (IEntityValidationService<T>)new PessoaContaValidationService();
-
-            if (typeof(T) == typeof(Contato))
-                return (IEntityValidationService<T>)new ContatoValidationService();
 
             if (typeof(T) == typeof(LogEntry))
                 return (IEntityValidationService<T>)new LogsValidationService();
