bcbada3ef4e7b54e9785b19edcf5305550922469
Gabriel Sanz <gabriel.sanz@infortechms.com.br>
2025-09-25 14:59:54 -0400

    Remover repositórios e mapeamentos de contatos e finanças
    As alterações no código removem injeções de dependência e definições de mapeamento para repositórios relacionados a contatos e finanças, como `IPessoaRepository` e `ICartaPokemonRepository`. Além disso, as definições de `DbSet` no `MySQLContext.cs` para entidades como `Contato`, `Cartao`, `Categoria`, `Lancamento`, `Parcelamento`, `ParcelamentoMensal` e `PessoaConta` foram eliminadas. Isso sugere uma reestruturação significativa do sistema, com a remoção de funcionalidades associadas a essas entidades.

--- Projeto_Gabriel.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Projeto_Gabriel.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Projeto_Gabriel.Domain.RepositoryInterface;
-using Projeto_Gabriel.Domain.RepositoryInterface.Contatos;
-using Projeto_Gabriel.Domain.RepositoryInterface.Financas;
 using Projeto_Gabriel.Domain.RepositoryInterface.Logas;
 using Projeto_Gabriel.Infrastructure.Repositorys;
-using Projeto_Gabriel.Infrastructure.Repositorys.Contatos;
-using Projeto_Gabriel.Infrastructure.Repositorys.Financas;
 using Projeto_Gabriel.Infrastructure.Repositorys.Logas;
 using Projeto_Gabriel.Repository.Generic;
 
@@ -16,20 +12,7 @@ namespace Projeto_Gabriel.Infrastructure.Extensions
         public static IServiceCollection AddInfrastructureRepositories(this IServiceCollection services)
         {
             services.AddScoped<IUsuarioRepository, UsuarioRepository>();
-            services.AddScoped<IPessoaRepository, PessoaRepository>();
             services.AddScoped<ILivroRepository, LivroRepository>();
-            services.AddScoped<ICartaPokemonRepository, CartaPokemonRepository>();
-
-            //Financas
-            services.AddScoped<ICartaoRepository, CartaoRepository>();
-            services.AddScoped<ICategoriaRepository, CategoriaRepository>();
-            services.AddScoped<IPessoaContaRepository, PessoaContaRepository>();
-            services.AddScoped<ILancamentoRepository, LancamentoRepository>();
-            services.AddScoped<IParcelamentoRepository, ParcelamentoRepository>();
-            services.AddScoped<IParcelamentoMensalRepository, ParcelamentoMensalRepository>();
-
-            //Contato
-            services.AddScoped<IContatoRepository, ContatoRepository>();
 
             //Logs
             services.AddScoped<ILogRepository, LogRepository>();
--- Projeto_Gabriel.Infrastructure/Migrations/MySQLContext.cs
+++ Projeto_Gabriel.Infrastructure/Migrations/MySQLContext.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Projeto_Gabriel.Domain.Entity;
-using Projeto_Gabriel.Domain.Entity.Contatos;
-using Projeto_Gabriel.Domain.Entity.Financas;
 using Projeto_Gabriel.Domain.Entity.Logas;
 
 namespace Projeto_Gabriel.Model.Context
@@ -15,25 +13,12 @@ namespace Projeto_Gabriel.Model.Context
 
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options){}
 
-        public DbSet<Pessoa> Pessoas { get; set; }
         public DbSet<Livros> Livros { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
-        public DbSet<CartaPokemon> CartasPokemon { get; set; }
 
-        //Finanças
-        public DbSet<Categoria> Categorias { get; set; }
-        public DbSet<Cartao> Cartoes { get; set; }
-        public DbSet<Lancamento> Lancamentos { get; set; }
-        public DbSet<Parcelamento> Parcelamentos { get; set; }
-        public DbSet<ParcelamentoMensal> ParcelamentoMensais { get; set; }
-        public DbSet<PessoaConta> PessoaContas { get; set; }
 
         //Logs
         public DbSet<LogEntry> Logs { get; set; }
-
-        //Contato 
-        public DbSet<Contato> Contatos { get; set; }
-
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
